7c276b89fd4d4fc8c8a3db58a58c4ac63852d9d9
Thibaut Hermant <hermantthibaut@gmail.com>
2020-05-10 01:37:21 +0200

    Template & table

---
+++ table.php
@@ -0,0 +1,367 @@
+<head>
+
+	<link rel="stylesheet" type="text/css" href="css/main_table.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+					<div class="table">
+
+						<div class="rowtable header">
+							<div class="cell">
+								Activité
+							</div>
+							<div class="cell">
+								Thème
+							</div>
+							<div class="cell">
+								Date
+							</div>
+							<div class="cell">
+								Lieu
+                            </div>
+                            <div class="text-center cell">
+								Heures réelles
+                            </div>
+                            <div class="text-center cell">
+								Heures valorisées
+                            </div>
+                            <div class="text-center cell">
+								Preuves et Infos
+							</div>
+						</div>
+
+						<div class="rowtable">
+							<div class="cell" data-title="Activite">
+								E-forum
+							</div>
+							<div class="cell" data-title="Theme">
+								Séminaire
+							</div>
+							<div class="cell" data-title="Date">
+								22 Octobre 2018
+							</div>
+							<div class="cell" data-title="Lieu">
+								Cercle du Lac, Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+								7h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								7h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+								JobIT Day
+							</div>
+							<div class="cell" data-title="Theme">
+								Salons
+							</div>
+							<div class="cell" data-title="Date">
+								9 Novembre 2018
+							</div>
+							<div class="cell" data-title="Lieu">
+								Charleroi
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+								6h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								6h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+								Conférence Intelligence Artificielle
+							</div>
+							<div class="cell" data-title="Theme">
+								Conférences
+							</div>
+							<div class="cell" data-title="Date">
+								25 Février 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Agora, Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Semaine international
+							</div>
+							<div class="cell" data-title="Theme">
+								Echanges international
+							</div>
+							<div class="cell" data-title="Date">
+								Avril 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Durham, Angleterre
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+								7 jours
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								10h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+						</div>
+                        <div class="rowtable">
+							<div class="cell pr-2" data-title="Activite">
+                                Conquerir un marché de niche: la solution du digital
+							</div>
+							<div class="cell" data-title="Theme">
+								Conférences
+							</div>
+							<div class="cell" data-title="Date">
+								29 Avril 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Agora, Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Wecodx
+							</div>
+							<div class="cell" data-title="Theme">
+								Entreprenariat
+							</div>
+							<div class="cell" data-title="Date">
+								Mai 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                ∞
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								10h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Voyage à Malte
+							</div>
+							<div class="cell" data-title="Theme">
+								Voyage culturel
+							</div>
+							<div class="cell" data-title="Date">
+								Septembre 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Malte
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                3 jours
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								10h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell pr-2" data-title="Activite">
+                                Developpez une application mobile en React Native
+							</div>
+							<div class="cell" data-title="Theme">
+								Formation
+							</div>
+							<div class="cell" data-title="Date">
+								Octobre 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                30h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								10h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Conférence Odoo
+							</div>
+							<div class="cell" data-title="Theme">
+								Conférence
+							</div>
+							<div class="cell" data-title="Date">
+								16 Octobre 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Ephec, Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                2h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								1h15
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                A la découverte de l'Intelligence Artificielle
+							</div>
+							<div class="cell" data-title="Theme">
+								Conférence
+							</div>
+							<div class="cell" data-title="Date">
+								21 Novembre 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Axisparc, Mont-saint-Guilbert
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                2h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Dev day
+							</div>
+							<div class="cell" data-title="Theme">
+								Salons
+							</div>
+							<div class="cell" data-title="Date">
+								26 Novembre 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Cinescope, Louvain-la-Neuve
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                10h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								10h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Utilisez des API REST dans vos projets web
+							</div>
+							<div class="cell" data-title="Theme">
+								Formation
+							</div>
+							<div class="cell" data-title="Date">
+								Avril 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Froidchapelle
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                6h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								6h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable">
+							<div class="cell" data-title="Activite">
+                                Atelier #2 : Vendre en ligne à l'international
+							</div>
+							<div class="cell" data-title="Theme">
+								Séminaire
+							</div>
+							<div class="cell" data-title="Date">
+								5 Mai 2019
+							</div>
+							<div class="cell" data-title="Lieu">
+								Froidchapelle
+                            </div>
+                            <div class="cell text-center" data-title="Heures reelles">
+                                2h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées">
+								2h
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+								<a href="#">infos</a>
+							</div>
+                        </div>
+                        <div class="rowtable header">
+							<div class="cell" data-title="Activite">
+                                TOTAL
+							</div>
+							<div class="cell" data-title="Theme">
+								
+							</div>
+							<div class="cell" data-title="Date">
+								
+							</div>
+							<div class="cell" data-title="Lieu">
+								
+                            </div>
+                            <div class="cell text-center" data-title="Heures réelles total">
+                                319 h
+                            </div>
+                            <div class="cell text-center" data-title="Heures valorisées total">
+								78h 15min
+                            </div>
+                            <div class="cell text-center" data-title="Infos">
+							</div>
+                        </div>
+
+					</div>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
